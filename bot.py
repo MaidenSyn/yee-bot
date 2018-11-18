1e79d5ce15b321a61e13758169d283c516cb73c0
CyberJelly <45150895+CyberJelly@users.noreply.github.com>
2018-11-18 19:46:30 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import time
+client = discord.Client()
+#prefix
+client = commands.Bot(client,commmand_prefix = "yee")
+#image
+yee = discord.Embed()
+yee.set_image(url="https://pbs.twimg.com/profile_images/504715443479670784/fauyuPDy_400x400.png")
+#online
+@client.event
+async def on_ready():
+    print("Ready")
+    print("I am " + client.user.name)
+    print("ID: " + client.user.id)
+@client.event
+async def on_message(message):
+    userID = message.author.id
+    if message.content.startswith("ye") and userID != "@Yee Bot#5127" :
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Alexa this is so sad can we play despacito" % (userID))
+        await client.send_message(message.channel, "https://pbs.twimg.com/profile_images/504715443479670784/fauyuPDy_400x400.png")
+    
+
+
+
+
+if __name__ == '__main__':
+    import config
+    client.run(config.token)
